22dd1272cf5f170b7b12eefa4cf81c6daa137be4
osuji chiaka <chiakajunior@yahoo.com>
2024-07-24 12:09:08 +0300

    working on react 18

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 const test = <p>Hello World</p>
 
-const root = document.createElement('div');
+// const root = document.createElement('div');
+// document.body.appendChild(root);
+// ReactDOM.(test, root);
+
+const root = ReactDOM.createRoot(document.createElement('div') as HTMLElement)
 document.body.appendChild(root);
-ReactDOM.render(test, root);
+root.render(test, root)
